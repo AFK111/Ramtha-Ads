06fbbc0bb4767b524cdb268cdf76ad4cf4698634
AFK1 <ahmadtheapat1@gmail.com>
2020-06-25 06:57:21 +0300

    Version1 is done

--- includes/templates/header.php
+++ includes/templates/header.php
@@ -6,6 +6,7 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?php getTitle(); ?></title>
+	<link rel="icon" type="image/ico" href="admin/layout/images/HiLogo.png">
 	<link href="https://fonts.googleapis.com/css?family=Courier+Prime&display=swap" rel="stylesheet"> <!-- google font-->
 	<link rel="stylesheet" href="<?php echo $css; ?>bootstrap.min.css">
 	<link rel="stylesheet" href="<?php echo $css; ?>font-awesome.min.css">
