6f4764dacc75fbe8f2105472cf92b1c88d099acc
AFK1 <ahmadtheapat1@gmail.com>
2020-06-27 05:26:30 +0300

    edit connect file add local and remote vars

--- admin/connect.php
+++ admin/connect.php
@@ -1,10 +1,19 @@
 <?php 
+
+
+	//local DB
+	// $dbName = 'shop';
+	// $host = 'localhost';
+	// $user = 'root';
+	// $pass='';
+
+	//Remote DB
 	$dbName = getenv('DB_NAME');
 	$host = getenv('DB_HOST');
+	$user = getenv('DB_USERNAME');
+	$pass=getenv('DB_PASS');
 
 	$dsn="mysql:host=$host;dbname=$dbName";
-	$user = getenv('DB_USERNAME');
-	$pass=getenv('DB_PASS1');
 	$option=array(
 		PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
 	);
